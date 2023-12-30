4a097135ff00ad987e0f662127cdfa29ab303736
cfgarciaq <cfgarciaq@gmail.com>
2023-12-30 20:48:45 +0100

    Update MainMenu.cs
    Added private members
    Added functions to load farm scene and get farms name from input field

--- Programming Theory Project/Assets/Scripts/MainMenu/MainMenu.cs
+++ Programming Theory Project/Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // .UI clasic UI system - .UIElements new UI Toolkit System
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private InputField farmNameField;
+    [SerializeField] private Button startButton;
+
     void Start()
     {
-        
+        if(farmNameField != null)
+        {
+            farmNameField.text = "";
+        }
+        else
+        {            
+            Debug.LogWarning($"farm's name input field not assigned, drag and drop it in editor");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReadTextfield()
     {
-        
+        string farm_name = farmNameField.text;
+        // to do: save farms name to static class
+
+        //DataManager.FarmName = farm_name;
+        //other stuff if needed
+    }
+
+    public void LoadFarmScene()
+    {
+        //get farm name and save it for the play session
+        SceneManager.LoadScene(1);
     }
 }
