0991315392e53b9f23c4d9370cefb13ebd73865d
cfgarciaq <cfgarciaq@gmail.com>
2023-12-31 03:01:04 +0100

    Update DataManager.cs
    Added static DataManager instance variable for data persistence
    Added DontDestroyOnLoad and checked if objet already existe to prevent duplicated objects when going back to the main menu scene
    
    Added in comment a save system exemple to use for later.......

--- Programming Theory Project/Assets/Scripts/DataManager.cs
+++ Programming Theory Project/Assets/Scripts/DataManager.cs
@@ -1,18 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static DataManager Instance;
+
+    public string FarmName { get; set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Save system to implement if needed
 }
+
+
+
+/* 
+     * SAVE SYSTEM CODE EXEMPLE 
+     * 
+        [System.Serializable]
+        private class BestPlayer
+        {
+            public string Name;
+            public int Score;
+        }
+
+        private void SaveScore()
+        {
+            try
+            {
+                BestPlayer best = new BestPlayer();
+                best.Name = BestPlayerName;
+                best.Score = HigestScore;
+
+                string json_bestScore = JsonUtility.ToJson(best);
+
+                File.WriteAllText($"{Application.persistentDataPath}/savedBestScore.json", json_bestScore);
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"Error: {e} \nCouldn't save json file");
+            }
+        }
+
+        private void LoadScore()
+        {
+            string path = $"{Application.persistentDataPath}/savedBestScore.json";
+
+            if (File.Exists(path))
+            {
+                string json_savedBestScore = File.ReadAllText(path);
+                BestPlayer bestPlayer = JsonUtility.FromJson<BestPlayer>(json_savedBestScore);
+
+                HigestScore = bestPlayer.Score;
+                BestPlayerName = bestPlayer.Name;
+            }
+            else
+            {
+                HigestScore = 0;
+                BestPlayerName = "NoHighScore";
+            }
+        }
+     *
+     */
