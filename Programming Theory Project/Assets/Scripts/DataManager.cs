8014027a7825f83a9c61c8b511972ddef8e4dc35
cfgarciaq <cfgarciaq@gmail.com>
2024-01-01 18:05:20 +0100

    Update DataManager.cs
    refactor awake function

--- Programming Theory Project/Assets/Scripts/DataManager.cs
+++ Programming Theory Project/Assets/Scripts/DataManager.cs
@@ -1,27 +1,24 @@
-
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-
     public static DataManager Instance;
-
     public string FarmName { get; set; }
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
+        if (Instance != null)
         {
             Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     // Save system to implement if needed
+
 }
 
 
