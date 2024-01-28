ec5790ef8ca381f83da955dc37bda9ecb7af46a5
cfgarciaq <cfgarciaq@gmail.com>
2024-01-28 17:08:23 +0100

    Update AnimalInfoPanel.cs
    Changed public function to a property for (better?) encapsulation

--- Programming Theory Project/Assets/Scripts/UI/AnimalInfoPanel.cs
+++ Programming Theory Project/Assets/Scripts/UI/AnimalInfoPanel.cs
@@ -8,27 +8,30 @@ public class AnimalInfoPanel : MonoBehaviour
     [SerializeField] private Text txt_animalType, txt_animalFeed, txt_animalHappiness;
     [SerializeField] private Button caressButton;
     
-    private AnimalData animalData = new AnimalData();
+    private AnimalData animalData = null;
 
-    // Start is called before the first frame update
+    //ENCAPSULATION
+    public AnimalData AnimalData
+    { 
+        set
+        {
+            animalData = value;
+        } 
+    }
+
     void Start()
     {
-        txt_animalFeed.text = "";
+        txt_animalType.text = "";
         txt_animalFeed.text = "";
         txt_animalHappiness.text = "";
     }
 
-    // Update is called once per frame
     void Update()
     {
-        txt_animalFeed.text = animalData.AnimalType;
+        
+        txt_animalType.text = animalData.AnimalType;
         txt_animalFeed.text = animalData.FeedStatus;
         txt_animalHappiness.text = animalData.HappinessStatus;
-    }
-
-    public void SetAnimalData(AnimalData data)
-    {
-        animalData = data;
     }
 }
 
